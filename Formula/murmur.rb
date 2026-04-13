d3135e825a28f7fe9bc28cf875fad54b319104b9
Joseph Bernstein <joe@prasso.ai>
2026-04-13 12:25:15 -0700

    Add murmur formula skeleton (v0.1.0)

---
+++ Formula/murmur.rb
@@ -0,0 +1,35 @@
+class Murmur < Formula
+  desc "CLI for the Murmur agent orchestration platform"
+  homepage "https://github.com/prassoai/murmuration"
+  version "0.1.0"
+
+  on_macos do
+    on_arm do
+      url "https://github.com/prassoai/murmuration/releases/download/v0.1.0/murmur-darwin-arm64"
+      sha256 "PLACEHOLDER"
+    end
+    on_intel do
+      url "https://github.com/prassoai/murmuration/releases/download/v0.1.0/murmur-darwin-amd64"
+      sha256 "PLACEHOLDER"
+    end
+  end
+
+  on_linux do
+    on_arm do
+      url "https://github.com/prassoai/murmuration/releases/download/v0.1.0/murmur-linux-arm64"
+      sha256 "PLACEHOLDER"
+    end
+    on_intel do
+      url "https://github.com/prassoai/murmuration/releases/download/v0.1.0/murmur-linux-amd64"
+      sha256 "PLACEHOLDER"
+    end
+  end
+
+  def install
+    bin.install stable.url.split("/").last => "murmur"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/murmur version")
+  end
+end
